e29e5dc7912310e362f959df50d6f424375924d2
Luciano Oliveira <luciano101@msn.com>
2017-06-29 10:45:47 -0400

    completed todo app

--- src/app.js
+++ src/app.js
@@ -3,20 +3,50 @@ var app = new Vue({
   data: {
     message: 'Hello Vue!',
     todo: null,
-    todos: []
+    editTodo: null,
+    todos: [
+    ]
   },
   methods: { 
   	add(todo){
   		if(!this.todoIsValid){
   			return;
   		}
-  		this.todos.push(todo);
+  		this.todos.push({
+				content: todo,
+				finished: false,
+				isEdited: false
+  		});
   		this.todo = null;
+  	},
+  	edit(todo){
+  		this.editTodo = todo.content;
+			todo.isEdited = true;
+  	},
+  	update(todo){
+  		if(!this.editTodoIsValid){
+  			return;
+  		}
+  		todo.content = this.editTodo;
+			todo.isEdited = false;
+  	},
+  	remove(todo){
+			this.todos = this.todos.filter((item) => {
+				return item != todo;
+			});
   	}
   },
   computed : {
   	todoIsValid(){
   		return !!this.todo;
+  	},
+  	editTodoIsValid(){
+  		return !!this.editTodo;
+  	},
+  	isBeingEdited(){
+  		return this.todos
+  			.filter(todo => todo.isEdited)
+  			.length > 0;
   	}
   }
 });
